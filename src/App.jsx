57b04923c3ac66899741fbaad367d86a0be2de62
Ibrahim Lasisi <ilasisi90@gmail.com>
2025-09-27 11:35:17 +0100

    add chat interactivity

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,45 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
+import { MessageBubble } from "./components/MessageBubble";
+import { autoReplies } from "./utils";
+
+//useRef
+//useEffect
 
 function App() {
+    const endOfChatRef = useRef(null);
     const [message, setMessage] = useState("");
+    const [conversations, setConversations] = useState([
+        {
+            sender: "bot",
+            text: "Hello Champs! How may I help you today?",
+            timestamp: new Date(),
+        },
+    ]);
 
     const sendMessage = () => {
         if (message.length === 0) return;
 
-        console.log(message);
+        const newMessage = {
+            sender: "user",
+            text: message,
+            timestamp: new Date(),
+        };
+
+        setConversations((prev) => [...prev, newMessage]);
         setMessage("");
+
+        setTimeout(() => {
+            const botMessage = {
+                sender: "bot",
+                text: autoReplies[
+                    Math.floor(Math.random() * autoReplies.length)
+                ],
+                timestamp: new Date(),
+            };
+
+            setConversations((prev) => [...prev, botMessage]);
+        }, 2000);
     };
 
     const handleKeyDown = (e) => {
@@ -17,6 +48,10 @@ function App() {
         }
     };
 
+    useEffect(() => {
+        endOfChatRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, [conversations]);
+
     return (
         <div className="bg-white w-full h-screen sm:w-[500px] sm:h-[500px] rounded-3xl overflow-hidden flex flex-col shadow-lg shadow-gray-200 border border-gray-300">
             <div className="bg-amber-600 px-5 py-4">
@@ -24,7 +59,17 @@ function App() {
                     NotZero Chat Room
                 </h1>
             </div>
-            <div className="flex-1"></div>
+            <div className="flex-1 space-y-3 p-5 overflow-y-auto">
+                {conversations.map((conversation, index) => (
+                    <MessageBubble
+                        key={index}
+                        message={conversation.text}
+                        timestamp={conversation.timestamp}
+                        sender={conversation.sender}
+                    />
+                ))}
+                <div ref={endOfChatRef}></div>
+            </div>
             <div className="p-5 flex justify-around border-t border-gray-200 gap-3">
                 <input
                     placeholder="Start typing ..."
