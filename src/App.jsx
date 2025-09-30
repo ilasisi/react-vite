7b2c34ec5faeb677c2af686c03bcfa5d237c1934
Ibrahim Lasisi <ilasisi90@gmail.com>
2025-09-30 17:47:28 +0100

    add form input

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,80 @@
 import { useState } from "react";
 import "./App.css";
-import { ArrowIcon } from "./ArrowIcon";
-
-const faqs = [
-    {
-        question: "What is UX design?",
-        answer: "UX design stands for User Experience design. It is the process of designing digital or physical products that are easy to use, intuitive, and enjoyable for the user.",
-    },
-    {
-        question: "What are the key principles of UX design?",
-        answer: "UX design stands for User Experience design. It is the process of designing digital or physical products that are easy to use, intuitive, and enjoyable for the user.",
-    },
-    {
-        question: "What is the difference between UX and UI design?",
-        answer: "UX design stands for User Experience design. It is the process of designing digital or physical products that are easy to use, intuitive, and enjoyable for the user.",
-    },
-    {
-        question: "What is a wireframe?",
-        answer: "UX design stands for User Experience design. It is the process of designing digital or physical products that are easy to use, intuitive, and enjoyable for the user.",
-    },
-    {
-        question: "What is user testing?",
-        answer: "UX design stands for User Experience design. It is the process of designing digital or physical products that are easy to use, intuitive, and enjoyable for the user.",
-    },
-];
 
 function App() {
-    const [selectedItem, setSelectedItem] = useState(0);
+    const [data, setData] = useState({
+        name: "",
+        accept: false,
+        gender: "",
+        category: "",
+        bio: "",
+    });
 
-    const toggleItem = (index) => {
-        setSelectedItem(selectedItem === index ? null : index);
+    const handleChange = (e) => {
+        const { type, name, value, checked } = e.target;
+
+        setData({ ...data, [name]: type === "checkbox" ? checked : value });
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        console.log(data);
     };
 
     return (
-        <div className="min-w-4xl bg-white p-5 space-y-6">
-            <p className="font-extrabold text-[40px]">
-                Frequently Asked Questions
-            </p>
-            <div className="max-w-3/4 mx-auto space-y-6">
-                {faqs.map((faq, index) => (
-                    <div key={index} className="bg-[#F9F5F2] space-y-3 p-5">
-                        <button
-                            className="w-full flex justify-between items-center gap-3"
-                            onClick={() => toggleItem(index)}
-                        >
-                            <span className="font-extrabold text-[30px]">
-                                {faq.question}
-                            </span>
-                            <span className="w-[40px] h-[40px] flex items-center justify-center border rounded-full">
-                                <ArrowIcon
-                                    className={`transform transition-transform duration-200
-                                        ${
-                                            selectedItem === index
-                                                ? "rotate-180"
-                                                : ""
-                                        }
-                                    `}
-                                />
-                            </span>
-                        </button>
-                        <div
-                            className={`transition-all duration-200 ease-in-out
-                                ${
-                                    selectedItem === index
-                                        ? "max-h-96 opacity-100"
-                                        : "max-h-0 opacity-0"
-                                }
-                            `}
-                        >
-                            <p className="text-left">{faq.answer}</p>
-                        </div>
-                    </div>
-                ))}
-            </div>
-        </div>
+        <form onSubmit={handleSubmit}>
+            <input
+                value={data.name}
+                type="text"
+                name="name"
+                onChange={handleChange}
+                placeholder="Enter your name"
+                required
+            />
+            <br />
+            <input
+                checked={data.accept}
+                type="checkbox"
+                name="accept"
+                id="accept"
+                onChange={handleChange}
+            />
+            <label htmlFor="accept">Accept?</label>
+            <br />
+            <label>
+                <input
+                    value={"Male"}
+                    type="radio"
+                    name="gender"
+                    onChange={handleChange}
+                />
+                Male
+            </label>
+            <label>
+                <input
+                    defaultChecked
+                    value={"Female"}
+                    type="radio"
+                    name="gender"
+                    onChange={handleChange}
+                />
+                Female
+            </label>
+            <br />
+            <select
+                value={data.category}
+                name="category"
+                onChange={handleChange}
+            >
+                <option>Residential</option>
+                <option>Hospital</option>
+                <option>Factory</option>
+            </select>
+            <br />
+            <textarea name="bio" value={data.bio} onChange={handleChange} />
+            <button className="block">Submit</button>
+        </form>
     );
 }
 
